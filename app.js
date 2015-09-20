9f3fa9b7f8ec10a775561bf6650b691abdcd9890
Avik Jain <avikjain12345@gmail.com>
2015-09-19 18:42:00 -0700

    Removed mongoose

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
-	mongoose = require('mongoose');
+	//mongoose = require('mongoose'),
 	users = {};
 
 server.listen(process.env.PORT);
